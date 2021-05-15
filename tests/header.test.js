9d0f51312ab2632f22fbb558c0fc8cfba3c091f9
Panos Anastasiadis <anastapd@gmail.com>
2021-05-15 07:22:20 +0100

    Created page class to wrap puppeteer functionality and login

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,22 +1,18 @@
-const puppeteer = require("puppeteer");
+const Page = require("./helpers/page");
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-    browser = await puppeteer.launch({
-        headless: false
-    });
-    page = await browser.newPage();
-
+    page = await Page.build();
     await page.goto("localhost:3000");
 });
 
 afterEach(async () => {
-    await browser.close();
+    await page.close();
 });
 
 test("The header has the correct text", async () => {
-    const text = await page.$eval("a.brand-logo", el => el.innerHTML);
+    const text = await page.getContentsOf("a.brand-logo");
     expect(text).toEqual("Blogster");
 });
 
@@ -28,35 +24,10 @@ test("Clicking login starts the oauth flow", async () => {
     expect(url).toMatch(/accounts\.google\.com/);
 });
 
-test.only("When signed in, shows logout button", async () => {
-    const id = "6097ad5cc782fc1096d61761";
-    const Buffer = require("safe-buffer").Buffer;
+test("When signed in, shows logout button", async () => {
+    await page.login();
 
-    const sessionObject = {
-        passport: {
-            user: id
-        }
-    };
-
-    const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
-        "base64"
-    );
-
-    const Keygrip = require("keygrip");
-    const keys = require("../config/keys");
-
-    const keygrip = new Keygrip([keys.cookieKey]);
-    const sig = keygrip.sign("session=" + sessionString);
-
-    console.log(sessionString, sig);
-
-    await page.setCookie({ name: "session", value: sessionString });
-    await page.setCookie({ name: "session.sig", value: sig });
-    await page.goto("localhost:3000");
-
-    await page.waitFor('a[href="/auth/logout"]');
-
-    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
+    const text = await page.getContentsOf('a[href="/auth/logout"]');
 
     expect(text).toEqual("Logout");
 });
